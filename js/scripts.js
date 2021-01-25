7fa967ce110a539692b42d0cd37e0a98bc9840e4
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-01-25 13:55:00 -0800

    Update JS to prevent click event handlers from attching to the talk messages.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,10 @@ $(document).ready(function() {
   $("button#hello").click(function() {
     $("ul#user").prepend("<li>Hello!</li>");
     $("ul#webpage").prepend("<li>Why hello there!");
-    $("li").click(function() {
+    $("ul#user").children("li").first().click(function() {
+      alert("hi");
+    });
+    $("ul#webpage").children("li").first().click(function() {
       alert("hi");
     });
   });
